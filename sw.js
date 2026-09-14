bba0bfe339978643b949e012b10ee705d814637a
ghushsite <anamul246899@gmail.com>
2026-09-14 18:31:53 +0600

    Implement service worker with caching strategy

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "ghushsite-v1";
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    self.clients.claim()
+  );
+});
+
+self.addEventListener("fetch", event => {
+
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+
+});
